0ed88e0e36ea1dcc46798d063f95e59e90b9b1cf
Lorenz Reinhart <lorenz.reinhart@stud-mail.uni-wuerzburg.de>
2017-04-18 16:44:44 +0200

    Updated onePlot and manyPlot 	Using black and white theme, adjusted text size, adjusted figure size 	Exporting pdf instead of png 	printing both switches in manyPlot
    	geändert:       Bandwidth_allClients_manyPlot/bandwidth_allClients_manyPlot.r
    	geändert:       Bandwidth_allClients_onePlot/bandwidth_allClients_onePlot.r

--- rScripts/Bandwidth_allClients_manyPlot/bandwidth_allClients_manyPlot.r
+++ rScripts/Bandwidth_allClients_manyPlot/bandwidth_allClients_manyPlot.r
@@ -16,7 +16,8 @@ args <- commandArgs(trailingOnly = TRUE)
 # default values
 # resolution of the time axis
 resolution <- 1
-fileName <- "./temp.csv"
+fileName1 <- "./s2.csv"
+fileName2 <- "./s4.csv"
 outFilePath <- "./out"
 
 if(length(args) >= 1){
@@ -24,58 +25,92 @@ if(length(args) >= 1){
 }
 if(length(args) >= 2){
   print(as.character(args[2]))
-  fileName <- as.character(args[2])
+  fileName1 <- as.character(args[2])
 }
 if(length(args) >= 3){
-  outFilePath <- as.character(args[3])
+  print(as.character(args[3]))
+  fileName2 <- as.character(args[3])
+}
+if(length(args) >= 4){
+  outFilePath <- as.character(args[4])
 }
 rm(args)
 
 
 # compute the bandwidth data
-bandwidthData <- computeBandwidth(fileName, resolution)
+bandwidthData1 <- computeBandwidth(fileName1, resolution)
+bandwidthData2 <- computeBandwidth(fileName2, resolution)
 
 # remove "bandwidthAll" column
-bandwidthData[["bandwidthAll"]] <- NULL
+bandwidthData1[["bandwidthAll"]] <- NULL
+bandwidthData2[["bandwidthAll"]] <- NULL
 
 # melt the results together
-bandwidthData <- melt(bandwidthData, id="time", variable.name="tpPorts", value.name = "bandwidth")
+bandwidthData1 <- melt(bandwidthData1, id="time", variable.name="tpPorts", value.name = "bandwidth")
+bandwidthData2 <- melt(bandwidthData2, id="time", variable.name="tpPorts", value.name = "bandwidth")
 
 # add factor for variable order
-bandwidthData$tpPorts <- factor(bandwidthData$tpPorts, levels=sort(levels(bandwidthData$tpPorts)))
+bandwidthData1$tpPorts <- factor(bandwidthData1$tpPorts, levels=sort(levels(bandwidthData1$tpPorts)))
+bandwidthData2$tpPorts <- factor(bandwidthData2$tpPorts, levels=sort(levels(bandwidthData2$tpPorts)))
 
 # get extra source and destination port columns
-bandwidthData[["src"]] <- strsplit(as.character(bandwidthData[["tpPorts"]]), ", ")
-bandwidthData[["src"]] <- sapply(bandwidthData[["src"]], function (x) x[2])
-bandwidthData[["dst"]] <- strsplit(as.character(bandwidthData[["tpPorts"]]), ", ")
-bandwidthData[["dst"]] <- sapply(bandwidthData[["dst"]], function (x) x[1])
+bandwidthData1[["src"]] <- strsplit(as.character(bandwidthData1[["tpPorts"]]), ", ")
+bandwidthData1[["src"]] <- sapply(bandwidthData1[["src"]], function (x) x[2])
+bandwidthData1[["dst"]] <- strsplit(as.character(bandwidthData1[["tpPorts"]]), ", ")
+bandwidthData1[["dst"]] <- sapply(bandwidthData1[["dst"]], function (x) x[1])
 
-lineColor <- colorRampPalette(c("blue", "red"))(length(unique(bandwidthData[, "tpPorts"])))
-fillColor <- colorRampPalette(c("lightblue4", "lightcoral"))(length(unique(bandwidthData[, "tpPorts"])))
+bandwidthData2[["src"]] <- strsplit(as.character(bandwidthData2[["tpPorts"]]), ", ")
+bandwidthData2[["src"]] <- sapply(bandwidthData2[["src"]], function (x) x[2])
+bandwidthData2[["dst"]] <- strsplit(as.character(bandwidthData2[["tpPorts"]]), ", ")
+bandwidthData2[["dst"]] <- sapply(bandwidthData2[["dst"]], function (x) x[1])
+
+# combine both frames
+bandwidthData1[["Switch"]] <- "s2"
+bandwidthData2[["Switch"]] <- "s4"
+bandwidthData <- rbind(bandwidthData1, bandwidthData2)
+# set measurement start time to zero
+timeMin <- min(bandwidthData[["time"]])
+bandwidthData[["time"]] <- sapply(bandwidthData[["time"]], function (x) {x-timeMin})
+
+# unify source port
+sourcePorts <- unique(bandwidthData[, "src"])
+i=1
+for(src in sourcePorts) {
+  bandwidthData[bandwidthData[["src"]]==src, "src"] <- sapply(bandwidthData[bandwidthData[["src"]]==src, "src"], function (x) {as.character(i)})
+  i <- i+1
+}
+rm(i)
+# define factor order
+bandwidthData$src <- factor(bandwidthData$src, levels=sort(as.integer(unique(bandwidthData$src))))
 
 for(destinationPort in unique(bandwidthData[, "dst"])) {
   
   # print the whole thing
   figure <- ggplot(data=bandwidthData[bandwidthData$dst==destinationPort,],
-                   aes(x=time, y=bandwidth, color=tpPorts, fill=tpPorts)) +
-    geom_area(size=0.3) +
-    facet_grid(dst + src ~ ., labeller=labeller(src = function(x) {paste("src=", x, sep="")}, dst = function(x) {paste("dst=", x, sep="")})) +
+                   aes(x=time, y=bandwidth, color=Switch, linetype=Switch)) +
+    geom_line() +
+  #  facet_grid(dst + src ~ ., labeller=labeller(src = function(x) {paste("src:", x, sep="")}, dst = function(x) {paste("dst:", x, sep="")})) +
+    facet_grid(src ~ ., labeller=labeller(src = function(x) {paste("src:", x, sep="")})) +
   #  scale_color_discrete(name = "TP-Ports (src, dst)") +
-    scale_color_manual(values=lineColor, name = "TP-Ports (src, dst)") +
+  #  scale_color_manual(values=lineColor, name = "TP-Ports (src, dst)") +
   #  scale_color_gradient(low="blue", high="red") +
-    scale_fill_manual(values=fillColor, name="TP-Ports (src, dst)") +
+  #  scale_fill_manual(values=fillColor, name="TP-Ports (src, dst)") +
+    scale_color_manual(values=c("blue", "red")) +
+    scale_linetype_manual(values=c("solid","42")) +
+    scale_y_continuous(breaks=c(0,100,200)) +
     xlab("Time (s)") + ylab("Bandwidth (kBit/s)") +
-    theme(legend.position = "none")
+    theme_bw() +
+    theme(legend.position = "bottom" , text = element_text(family="Helvetica", size=12))
   #  ggtitle(basename(outFilePath))
   
   # save plot as png
   #width <- 7.9; height <- 3.5
   #width <- 2.9; height <- 2.0
-  width <- 5.9; height <- 2.0 * length(unique(bandwidthData[bandwidthData[["dst"]]==destinationPort, "src"]))
-  ggsave(paste(outFilePath, as.character(destinationPort), ".png", sep=""), plot = figure, width = width, height = height)
+  width <- 7.4; height <- 1.0 + 1.8 * length(unique(bandwidthData[bandwidthData[["dst"]]==destinationPort, "src"]))
+  ggsave(paste(outFilePath, as.character(destinationPort), ".pdf", sep=""), plot = figure, width = width, height = height, units="cm")
   
   # update color values to use for next figure
-  srcNum <- length(unique(bandwidthData[bandwidthData[["dst"]]==destinationPort, "src"]))
-  lineColor <- tail(lineColor, -srcNum)
-  fillColor <- tail(fillColor, -srcNum)
+  #srcNum <- length(unique(bandwidthData[bandwidthData[["dst"]]==destinationPort, "src"]))
+  #lineColor <- tail(lineColor, -srcNum)
+  #fillColor <- tail(fillColor, -srcNum)
 }
--- rScripts/Bandwidth_allClients_onePlot/bandwidth_allClients_onePlot.r
+++ rScripts/Bandwidth_allClients_onePlot/bandwidth_allClients_onePlot.r
@@ -17,7 +17,7 @@ args <- commandArgs(trailingOnly = TRUE)
 # resolution of the time axis
 resolution <- 1
 fileName <- "temp.csv"
-outFilePath <- "./out.png"
+outFilePath <- "./out.pdf"
 
 if(length(args) >= 1){
 	resolution <- as.numeric(args[1])
@@ -38,22 +38,28 @@ bandwidthData <- bandwidthData[,c("time","bandwidthAll")]
 # melt the results together
 #bandwidthData2 <- melt(bandwidthData, id="time", variable.name="tpPorts", value.name = "bandwidth")
 
+# set measurement start time to zero
+timeMin <- min(bandwidthData[["time"]])
+bandwidthData[["time"]] <- sapply(bandwidthData[["time"]], function (x) {x-timeMin})
+
 #lineColor <- c("black", colorRampPalette(c("blue", "red"))(length(unique(bandwidthData[, "tpPorts"]))))
 
 # print the whole thing
 a <- ggplot(data=bandwidthData, aes(x=time, y=bandwidthAll)) +
-  geom_line(size=0.2) +
+  geom_line() +
 # scale_color_gradient(low="coral", high="steelblue", name = "TP-Ports (src, dst)") +
 # scale_color_brewer(palette="Dark2") +
 # scale_color_manual(values=lineColor, name = "TP-Ports (src, dst)") +
-  xlab("Time (s)") + ylab("Bandwidth (kBit/s)")
+  xlab("Time (s)") + ylab("Bandwidth (kBit/s)") +
+  theme_bw() +
+  theme(text = element_text(size=12))
 # ggtitle(basename(outFilePath))
 #print(a)
 
 # save cdf_plot as pdf
 #width <- 5.9; height <- 3.5
-width <- 7.9; height <- 3.5
+#width <- 7.9; height <- 3.5
+width <- 15.0; height <- 7.0
 #width <- 2.9; height <- 2.0
 #plot <- plot + theme(plot.margin = grid::unit(c(0,0,0,0), "mm"))
-ggsave(outFilePath, plot = a, width = width, height = height)
-
+ggsave(outFilePath, plot = a, width = width, height = height, units="cm")
