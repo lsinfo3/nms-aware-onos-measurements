cb5c2f62d0bf3780a4f3fdd1dfeaa1ba428adf20
Lorenz Reinhart <lorenz.reinhart@stud-mail.uni-wuerzburg.de>
2017-04-19 11:07:29 +0200

    Updated diffPlot one and three 	sourced the bandwidth calculation out to the function mergeBandwidth.r 	removed command line argument "resolution"
    	geändert:       Bandwidth_allClients_diffPlot_one/bandwidth_allClients_diffPlot_one.r
    	geändert:       Bandwidth_allClients_diffPlot_three/bandwidth_allClients_diffPlot_three.r
    	neue Datei:     mergeBandwidth.r

--- rScripts/Bandwidth_allClients_diffPlot_one/bandwidth_allClients_diffPlot_one.r
+++ rScripts/Bandwidth_allClients_diffPlot_one/bandwidth_allClients_diffPlot_one.r
@@ -8,6 +8,7 @@ rm(list=ls())
 
 
 source("/home/lorry/Masterthesis/vm/leftVm/python/rScripts/computeBandwidth.r")
+source("/home/lorry/Masterthesis/vm/leftVm/python/rScripts/mergeBandwidth.r")
 
 
 # get commandline args
@@ -21,72 +22,20 @@ fileName2 <- "./s4.csv"
 outFilePath <- "./out_diff"
 
 if(length(args) >= 1){
-  resolution <- as.numeric(args[1])
+  print(as.character(args[1]))
+  fileName1 <- as.character(args[1])
 }
 if(length(args) >= 2){
   print(as.character(args[2]))
-  fileName1 <- as.character(args[2])
+  fileName2 <- as.character(args[2])
 }
 if(length(args) >= 3){
-  print(as.character(args[3]))
-  fileName2 <- as.character(args[3])
-}
-if(length(args) >= 4){
-  outFilePath <- as.character(args[4])
+  outFilePath <- as.character(args[3])
 }
 rm(args)
 
 
-# compute the bandwidth data
-bandwidthData1 <- computeBandwidth(fileName1, resolution)
-bandwidthData2 <- computeBandwidth(fileName2, resolution)
-
-# remove "bandwidthAll" column
-bandwidthData1[["bandwidthAll"]] <- NULL
-bandwidthData2[["bandwidthAll"]] <- NULL
-
-# melt the results together
-bandwidthData1 <- melt(bandwidthData1, id="time", variable.name="tpPorts", value.name = "bandwidth")
-bandwidthData2 <- melt(bandwidthData2, id="time", variable.name="tpPorts", value.name = "bandwidth")
-
-# add factor for variable order
-bandwidthData1$tpPorts <- factor(bandwidthData1$tpPorts, levels=sort(levels(bandwidthData1$tpPorts)))
-bandwidthData2$tpPorts <- factor(bandwidthData2$tpPorts, levels=sort(levels(bandwidthData2$tpPorts)))
-
-# extract source and destination port as columns
-bandwidthData1[["src"]] <- strsplit(as.character(bandwidthData1[["tpPorts"]]), ", ")
-bandwidthData1[["src"]] <- sapply(bandwidthData1[["src"]], function (x) x[2])
-bandwidthData1[["dst"]] <- strsplit(as.character(bandwidthData1[["tpPorts"]]), ", ")
-bandwidthData1[["dst"]] <- sapply(bandwidthData1[["dst"]], function (x) x[1])
-
-bandwidthData2[["src"]] <- strsplit(as.character(bandwidthData2[["tpPorts"]]), ", ")
-bandwidthData2[["src"]] <- sapply(bandwidthData2[["src"]], function (x) x[2])
-bandwidthData2[["dst"]] <- strsplit(as.character(bandwidthData2[["tpPorts"]]), ", ")
-bandwidthData2[["dst"]] <- sapply(bandwidthData2[["dst"]], function (x) x[1])
-
-# combine both frames
-bandwidthData1[["Switch"]] <- "s2"
-bandwidthData2[["Switch"]] <- "s4"
-bandwidthData <- rbind(bandwidthData1, bandwidthData2)
-# set measurement start time to zero
-timeMin <- min(bandwidthData[["time"]])
-bandwidthData[["time"]] <- sapply(bandwidthData[["time"]], function (x) {x-timeMin})
-rm(timeMin)
-
-# simplify source port number
-i <- 1
-destinationPorts <- unique(bandwidthData[, "dst"])
-for(dst in sort(as.integer(destinationPorts))) {
-  sourcePorts <- unique(bandwidthData[bandwidthData[["dst"]]==dst, "src"])
-  for(src in sourcePorts) {
-    bandwidthData[bandwidthData[["dst"]]==dst & bandwidthData[["src"]]==src, "src"] <- sapply(bandwidthData[bandwidthData[["dst"]]==dst & bandwidthData[["src"]]==src, "src"], function (x) {as.character(i)})
-    i <- i+1
-  }
-}
-rm(i, src, dst, destinationPorts, sourcePorts)
-
-# define factor order
-bandwidthData$src <- factor(bandwidthData$src, levels=sort(as.integer(unique(bandwidthData$src))))
+bandwidthData <- mergeBandwidth(fileName1, fileName2, resolution)
 
 # print the whole thing
 figure <- ggplot(data=bandwidthData, aes(x=time, y=bandwidth, color=Switch, linetype=Switch)) +
