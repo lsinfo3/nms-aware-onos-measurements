0bd1142eef45fcd56aed7a6facf58e16d989166c
Lorenz Reinhart <lorenz.reinhart@stud-mail.uni-wuerzburg.de>
2017-05-22 09:55:48 +0200

    Updated runIperf.sh 	Added inter arrival time, bandwidth and flow duration variable. 	Start times for connections according to a neg exponential distribution function.
    	geändert:       iperfParameter/runIperf.sh

--- measurements/02_lowBandwidthSsh/iperfParameter/runIperf.sh
+++ measurements/02_lowBandwidthSsh/iperfParameter/runIperf.sh
@@ -2,16 +2,33 @@
 
 leftVmPath="/home/lorry/Masterthesis/vm/leftVm/"
 
+IAT="0"
+BANDWIDTH="400"
 COUNT="1"
 DURATION="120"
-BANDWIDTH="400"
-ARRIVAL="0"
+FLOWDUR=$DURATION
 USEUDP=false
 TYPE="ORG"
+runCommand="runIperf.sh [-i <inter arrival time in seconds>] \
+[-b <bandwidth per flow in kbit/s>] [-l <duration per flow>] \
+[-c <number of flows per iPerf instance>] [-d <overall measurement duration in seconds>] \
+[-u] -t {ORG|MOD|NMS}"
 
-while getopts "c:d:b:a:ut:h" opt; do
+while getopts "i:b:l:c:d:ut:h" opt; do
   case $opt in
-	  c)
+	i)
+      #echo "Flow inter arrival time: $OPTARG seconds" >&2
+      IAT=$OPTARG
+      ;;
+	b)
+      #echo "IPerf connection bandwidth: $OPTARG kbit/s" >&2
+      BANDWIDTH=$OPTARG
+      ;;
+    l)
+      #echo "Flow duration: $OPTARG" >&2
+      FLOWDUR=$OPTARG
+      ;;
+    c)
       #echo "Connection count: $OPTARG" >&2
       COUNT=$OPTARG
       ;;
@@ -19,32 +36,24 @@ while getopts "c:d:b:a:ut:h" opt; do
       #echo "Measurement duration: $OPTARG seconds" >&2
       DURATION=$OPTARG
       ;;
-    b)
-      #echo "IPerf connection bandwidth: $OPTARG kbit/s" >&2
-      BANDWIDTH=$OPTARG
-      ;;
-    a)
-	  echo "Interarrival time: $OPTARG s" >&2
-      ARRIVAL=$OPTARG
-      ;;
     u)
       #echo "Use UDP rather than TCP." >&2
       USEUDP=true
       ;;
     t)
-	    TYPE=$OPTARG
-	    if [ "$TYPE" == "ORG" ] || [ "$TYPE" == "MOD" ] || [ "$TYPE" == "NMS" ]
-		  then
-		    #echo "Measurement type: $OPTARG" >&2
-		    # no-op command is :
-		    :
-		  else
-		    echo "Measurement type not valid!"
-		    exit 1
-	    fi
-	    ;;
-	  h)
-      echo -e "Usage:\nconnectionNumber.sh [-c COUNT] [-d DURATION] [-b BANDWIDTH] [-u] -t {ORG|MOD|NMS}"
+	  TYPE=$OPTARG
+	  if [ "$TYPE" == "ORG" ] || [ "$TYPE" == "MOD" ] || [ "$TYPE" == "NMS" ]
+		then
+		  #echo "Measurement type: $OPTARG" >&2
+		  # no-op command is :
+		  :
+		else
+		  echo "Measurement type not valid!"
+	      exit 1
+	  fi
+	  ;;
+	h)
+      echo -e "Usage:\n$runCommand"
       exit 1
       ;;
     \?)
@@ -57,38 +66,53 @@ while getopts "c:d:b:a:ut:h" opt; do
       ;;
   esac
 done
+unset runCommand
+
 
 createCommand ()
 {
+	conNum=$1
+	serverPort=$2
+	
 	iperfCommand="bash -c \"cd $leftVmPath;"
 	iperfCommand="$iperfCommand vagrant ssh -c"
 	iperfCommand="$iperfCommand '/home/ubuntu/python/measurements/02_lowBandwidthSsh/testOverSsh.py"
-	iperfCommand="$iperfCommand -d $DURATION -c $3 -b $BANDWIDTH"
+	iperfCommand="$iperfCommand -d $FLOWDUR -c $COUNT -b $BANDWIDTH"
 	if [ "$USEUDP" == true ]; then
 	  iperfCommand="$iperfCommand -u"
 	fi
 	if [ "$TYPE" == "NMS" ]; then
 	  iperfCommand="$iperfCommand -a"
 	fi
-	iperfResultName="iperfResult${1}.txt"
-	iperfCommand="$iperfCommand -p ${2} -n iperf${1}"
+	iperfResultName="iperfResult${conNum}.txt"
+	iperfCommand="$iperfCommand -p $serverPort -n iperf${conNum}"
 	iperfCommand="$iperfCommand -r /home/ubuntu/${iperfResultName};"
 	iperfCommand="$iperfCommand cp /home/ubuntu/${iperfResultName} ./captures/${iperfResultName}'\""
 }
 
 
-if [ "$ARRIVAL" == "0" ]
-then
-	createCommand "1" "5001" $COUNT
+if [ "$IAT" == "0" ]; then
+	createCommand "1" "5001"
 	# execute command and move iperf result to captures folder at the end
 	gnome-terminal -e "$iperfCommand"
 	unset iperfCommand
-	
 else
-	for conNum in `seq 1 $COUNT`; do
-		createCommand $conNum $((5000 + $conNum)) "1"
+	timeCounter=0
+	counter=1
+	# run as long as the measurement time is not over
+	while [ $(echo "$timeCounter < $DURATION" | bc -l) == 1 ]; do
+		# calculate next connection start in seconds
+		# (negative exponential distribution function)
+		nextTime=$(bc -l <<< "-l(1.0 - $RANDOM/32767.0) * $IAT")
+		# linux supports floating point numbers but solaris not
+		sleep $nextTime
+		timeCounter=$(bc -l <<< "$timeCounter + $nextTime")
+	
+		printf "Time: %s\tCounter: %s\tServer Port: %s\tAverage IAT: %s\n" "$timeCounter" \
+		"$counter" "$((5000 + $counter))" "$(bc -l <<< "$timeCounter / $counter")"
+		createCommand $counter $((5000 + $counter))
 		gnome-terminal -e "$iperfCommand"
+		counter=$(($counter + 1))
 		unset iperfCommand
-		sleep $ARRIVAL
 	done
 fi
