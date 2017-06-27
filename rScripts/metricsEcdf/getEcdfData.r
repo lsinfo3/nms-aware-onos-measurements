6e9e14a4b3b9cab3f5426e7d6aed89e4ad719bb2
Lorenz Reinhart <lorenz.reinhart@stud-mail.uni-wuerzburg.de>
2017-06-27 13:46:04 +0200

    Added script to create reallocation and cpu load plot Added script to compute ecdf data
    	geändert:       ../../.gitignore
    	neue Datei:     getEcdfData.r
    	neue Datei:     reallocations_cpuLoad/metricsEcdfReallocationsCpuLoad.r

---
+++ rScripts/metricsEcdf/getEcdfData.r
@@ -0,0 +1,148 @@
+#!/usr/bin/env Rscript
+
+
+getEcdfData <- function(detail, folderName, folders, numMeas, parameterName) {
+
+if(detail == FALSE) {
+  for(i in folders) {
+    for(j in seq(1, 1)) {
+      if(exists("tempFiles")) {
+        tempFiles <- c(tempFiles, paste(folderName, "/", i, "/metrics.csv", sep=""))
+      } else {
+        tempFiles <- c(paste(folderName, "/", i, "/metrics.csv", sep=""))
+      }
+    }
+    if(exists("tempValues")) {
+      tempValues <- c(tempValues, rep(toString(i), 1))
+    } else {
+      tempValues <- c(rep(toString(i), 1))
+    }
+  }
+}
+
+#tempFiles <- c("10/1.csv", "10/2.csv", "10/3.csv", "10/4.csv", "10/5.csv", "10/6.csv", "10/7.csv", "10/8.csv", "10/9.csv", "10/10.csv")
+#tempValues <- c("10", "10", "10", "10", "10", "10", "10", "10", "10", "10")
+
+if(detail == TRUE) {
+  for(i in folders) {
+    for(j in seq(1, numMeas)) {
+      if(exists("tempFiles")) {
+        tempFiles <- c(tempFiles, paste(folderName, "/", i, "/", j, "/metrics_detail.csv", sep=""))
+      } else {
+        tempFiles <- c(paste(folderName, "/", i, "/", j, "/metrics_detail.csv", sep=""))
+      }
+    }
+    if(exists("tempValues")) {
+      tempValues <- c(tempValues, rep(toString(i), numMeas))
+    } else {
+      tempValues <- c(rep(toString(i), numMeas))
+    }
+  }
+}
+
+# get load measurement file names
+for(i in folders) {
+  for(j in seq(1, numMeas)) {
+    if(exists("tempLoadFiles")) {
+      tempLoadFiles <- c(tempLoadFiles, paste(folderName, "/", i, "/", j, "/systemLoad.csv", sep=""))
+    } else {
+      tempLoadFiles <- c(paste(folderName, "/", i, "/", j, "/systemLoad.csv", sep=""))
+    }
+  }
+  if(exists("tempLoadValues")) {
+    tempLoadValues <- c(tempLoadValues, rep(toString(i), numMeas))
+  } else {
+    tempLoadValues <- c(rep(toString(i), numMeas))
+  }
+}
+
+csvFiles <- tempFiles
+values <- tempValues
+loadCsv <- tempLoadFiles
+loadValues <- tempLoadValues
+rm(tempFiles, tempLoadFiles, tempValues, tempLoadValues, i, j, folders, numMeas)
+
+
+# get load measurement values
+for(i in 1:length(loadCsv)) {
+  loadPart <- read.csv(loadCsv[i], header=TRUE, sep=",", quote="\"", dec=".", fill=TRUE)
+  loadPart[["parameter"]] <- loadValues[i]
+  
+  # remove non convertible time columns
+  loadPart <- loadPart[ !is.na(as.numeric(loadPart[, "time"])), ]
+  
+  # normalize time values to begin with 0
+  minTime = min(floor(as.numeric(loadPart[, "time"])))
+  loadPart[, "time"] <- floor(as.numeric(loadPart[, "time"])) - minTime
+  
+  # change onosLoad data into long format
+  colnames(loadPart)[colnames(loadPart)=="cpu"] <- "cpuLoad"
+  loadPart <- melt(loadPart,
+                   id.vars=c("parameter", "time"),
+                   measure.vars="cpuLoad")
+  loadPart <- loadPart[, c("parameter", "variable", "value")]
+  
+  # build average if no detail information is wished
+  if(detail==FALSE) {
+    loadPart <- data.frame("parameter"=loadValues[i],
+                           "variable"="cpuLoad",
+                           "value"=mean(loadPart[, "value"], na.rm=TRUE))
+  }
+  
+  if(exists("onosLoad")) {
+    onosLoad <- rbind(onosLoad, loadPart)
+  } else {
+    onosLoad <- loadPart
+  }
+}
+rm(loadPart)
+
+# combine all csv data into wide format
+for(i in 1:length(csvFiles)) {
+  metricsPart <- read.csv(csvFiles[i], header=TRUE, sep=",", quote="\"", dec=".", fill=TRUE)
+  metricsPart[["parameter"]] <- values[i]
+  
+  if(exists("metrics")) {
+    metrics <- rbind(metrics, metricsPart)
+  } else {
+    metrics <- metricsPart
+  }
+}
+rm(metricsPart)
+
+rm(csvFiles, loadCsv, values, loadValues)
+
+
+metrics2 <- metrics
+# normalize reallocations
+#metrics[, "reallocations"] <- (1/(metrics[, "reallocations"]+1))^(1/10)
+#metrics[, "reallocations"] <- (1/(metrics[, "reallocations"]+1))
+#metrics[["reallocations"]] <- NULL
+
+# combine data into long format
+metrics <- melt(metrics[2:ncol(metrics)], measure.vars=1:(ncol(metrics)-2), id="parameter")
+metrics <- rbind(metrics, onosLoad)
+
+# remove rows with NA values
+metrics <- metrics[ !is.na(metrics[["value"]]), ]
+
+# set max value to 1
+metrics[metrics[["value"]] > 1 & metrics[["variable"]] != "reallocations", "value"] <- 1
+# set levels of dataframe
+if(detail==TRUE) {
+  
+}
+
+labels <- c(throughput="Throughput",
+            linkFairness="Link Fairness",
+            flowFairness="Flow Fairness")
+if(detail==TRUE) {
+  labels <- c(labels, cpuLoad="CPU Load")
+} else {
+  labels <- c(labels, reallocations="Reallocations", cpuLoad="CPU Load")
+}
+levels(metrics$variable) <- labels
+#setattr(metrics$variable, "levels", labels)
+
+return(metrics)
+}
