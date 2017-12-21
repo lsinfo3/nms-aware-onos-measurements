eb4b32cda309c6c18cc1b1e4d59f5ffd96911721
Lorenz Reinhart <lorenz-reinhart@web.de>
2017-12-21 14:03:18 +0100

    Update path of onos vm folder

--- python/measurements/02_lowBandwidthSsh/startMetricsMeasurement.sh
+++ python/measurements/02_lowBandwidthSsh/startMetricsMeasurement.sh
@@ -101,6 +101,7 @@ echo "Duration per flow: $FLOWDUR s." >&2
 
 
 # create results folder with date and time
+onosVmFolder="../../../vagrant/onos"
 leftVmFolder="../../../vagrant/nms"
 STARTTIME=$(date +%F_%H-%M-%S)
 resultFolder="$leftVmFolder/captures/${TYPE}_${STARTTIME}"
@@ -350,15 +351,14 @@ mv ${resultFolder}/*.csv $dataFolder
 # move metrics back to parent folder
 mv ${dataFolder}/metrics.csv $resultFolder
 
-# move onos load results
-onosVmFolder="$HOME/Masterthesis/vm/firstOnosVm"
-mv $onosVmFolder/*.csv $dataFolder
+# move onos load results (folder is defined at the beginning of the document)
+mv ${onosVmFolder}/*.csv $dataFolder
 unset onosVmFolder
 
 # remove capture files
-#rm $resultFolder/*.cap
+rm ${resultFolder}/*.cap
 # move capture files to measurement folder
-mv $resultFolder/*.cap $dataFolder
+#mv ${resultFolder}/*.cap $dataFolder
 
 unset fileBaseName fileName fileName2 fileFolderName dataFolder
 
